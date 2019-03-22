d788319a7c8249a1f52c6d3e4831d6895ebe0488
huhaifeng <l>
2019-03-22 11:12:38 +0800

    调度逻辑修改

--- flowable/src/main/java/com/hhf/flowable/service/impl/FlowableServiceImpl.java
+++ flowable/src/main/java/com/hhf/flowable/service/impl/FlowableServiceImpl.java
@@ -3,6 +3,7 @@ package com.hhf.flowable.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.fndsoft.flowable.service.ProcessDefinitionService;
 import com.hhf.common.exception.BusinessException;
+import com.hhf.common.utils.JacksonUtils;
 import com.hhf.flowable.constant.ProcessConstant;
 import com.hhf.flowable.constant.ProcessThreadLocal;
 import com.hhf.flowable.dto.CompleteTaskReqDTO;
@@ -18,6 +19,8 @@ import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.task.api.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,6 +40,7 @@ import java.util.Map;
 @Service
 public class FlowableServiceImpl implements FlowableService {
 
+    private static final Logger log = LoggerFactory.getLogger(FlowableServiceImpl.class);
     @Autowired
     ProcessDefinitionService processDefinitionService;
     @Autowired
@@ -82,8 +86,13 @@ public class FlowableServiceImpl implements FlowableService {
         map.put("auditReason", completeTaskReqDTO.getAuditReason());
         map.put("taskLevel", Integer.valueOf(uwTaskDetail.getTaskLevel()));
         if (completeTaskReqDTO.getHandlerDTO() != null) {
+            log.info("审核任务时设置了下一任务处理人，下一任务处理人为：{}", JacksonUtils.obj2json(completeTaskReqDTO.getHandlerDTO()));
             //设置本地变量   将下一任务处理人放入本地变量中，交给监听器处理
             taskService.setVariables(task.getId(), com.hhf.common.utils.BeanUtils.objectToMap(completeTaskReqDTO.getHandlerDTO()));
+            ProcessDispatchDTO processDispatchDTO = new ProcessDispatchDTO();
+            processDispatchDTO.setHandlerDTO(completeTaskReqDTO.getHandlerDTO());
+            log.info("--将下一任务处理人放入线程变量中--");
+            ProcessThreadLocal.setProcessDispatchDTO(processDispatchDTO);
         }
         taskService.complete(completeTaskReqDTO.getTaskProcessId(), map);
         ProcessInstance pi = runtimeService.createProcessInstanceQuery().processInstanceId(task.getProcessInstanceId()).singleResult();
@@ -95,9 +104,9 @@ public class FlowableServiceImpl implements FlowableService {
 
     @Override
     public void setAssignee(HandlerReqDTO handlerReqDTO) {
-
         ProcessDispatchDTO processDispatchDTO = new ProcessDispatchDTO();
         processDispatchDTO.setHandlerDTO(handlerReqDTO.getHandlerDTO());
+        log.info("领取任务，将处理人放入线程变量中，处理人为：{}",JacksonUtils.obj2json(handlerReqDTO.getHandlerDTO()));
         ProcessThreadLocal.setProcessDispatchDTO(processDispatchDTO);
         taskService.setAssignee(handlerReqDTO.getProcessTaskId(),handlerReqDTO.getHandlerDTO().getHandlerNo());
     }
